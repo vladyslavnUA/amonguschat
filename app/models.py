ba3801b2481050407f3da80978448cbfe9d8063e
vladyslavnUA <vladyslavn@yahoo.com>
2020-10-14 01:54:28 -0400

    contact model

--- app/models.py
+++ app/models.py
@@ -14,3 +14,11 @@ class Discussion(models.Model):
  
     def __str__(self):
         return str(self.forum)
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    message = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return str(self.name)
